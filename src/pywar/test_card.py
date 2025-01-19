88b88b66ecd62ee254798b43a59a8be4ed76acd9
Dainius Mikalajūnas <dainius@mklns.com>
2025-01-19 00:20:30 +0000

    Add less than operator support

--- src/pywar/test_card.py
+++ src/pywar/test_card.py
@@ -28,3 +28,26 @@ def test_card_can_be_instantiated_with_correct_rank_and_suite(rank: Rank, suit: 
 
     assert card.rank == rank
     assert card.suit == suit
+
+
+@pytest.mark.parametrize(
+    "first_card, second_card, expected",
+    [
+        (
+            Card(Rank.TWO, Suit.CLUBS),
+            Card(Rank.ACE, Suit.CLUBS),
+            True,
+        ),
+        (
+            Card(Rank.ACE, Suit.CLUBS),
+            Card(Rank.TWO, Suit.CLUBS),
+            False,
+        ),
+    ],
+)
+def test_card_less_than_operator_returns_correct_result(
+    first_card: Card,
+    second_card: Card,
+    expected: bool,
+):
+    assert (first_card < second_card) == expected
